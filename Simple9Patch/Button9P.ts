44af5b45390a5721e2fd9ee7142a55180bafb3a9
mf <mfukushima@mbn.nifty.com>
2015-02-02 22:43:58 +0900

    test commit

---
+++ Simple9Patch/Button9P.ts
@@ -0,0 +1,171 @@
+﻿module WebSketch {
+
+    /*  Simple 9 patch Button */
+    export class Button9P extends Phaser.Group {
+        splite: Phaser.Sprite[] = [];
+        textLabel: Phaser.Text;
+        btn: Phaser.Button;
+        lefttopRatio = new Phaser.Point(1 / 3, 1 / 3);
+        rightbottomRatio = new Phaser.Point(1 / 3, 1 / 3);
+        baseSize: Phaser.Point;
+        overFrame: number;
+        outFrame: number;
+        downFrame: number;
+        upFrame: number;
+
+        buttonCallback: (btn: Phaser.Button, pt: Phaser.Pointer) => void;
+
+        constructor(game: Phaser.Game, x: number, y: number, key: string, frame: number= 0,
+            callback: (btn: Phaser.Button, pt: Phaser.Pointer) => void= null, overFrame: number= 0, outFrame: number= 0, downFrame: number= 0, upFrame: number= 0) {
+            super(game);
+            this.overFrame = overFrame;
+            this.outFrame = outFrame;
+            this.downFrame = downFrame;
+            this.upFrame = upFrame;
+
+            this.buttonCallback = callback;
+            this.position.set(x, y);
+
+            //  cropしながらの9patchスプライトを割り当てる
+            //  透過パネルでボタンを割り当てて、押下時とフロー時のframe切り替えはそのボタンからのイベントでやる形を考えてみる
+            //  コンテンツのサイズが確定しないのでまずは割り当ててからサイズと位置とクロップを指定する
+            //  単純化のためバウンドのサイズを計算しておく
+            for (var j = 0; j < 3; j++) {
+                for (var i = 0; i < 3; i++) {
+                    var sp = new Phaser.Sprite(game, 0, 0, key);
+                    this.splite.push(sp);
+                    this.add(sp);
+                }
+            }
+            this.baseSize = new Phaser.Point(this.splite[0].width, this.splite[0].height);
+
+            this.btn = new Phaser.Button(game, 0, 0, null, null, null);
+
+            this.add(this.btn);
+            this.btn.onInputDown.add(this.btnDown, this);
+            this.btn.onInputUp.add(this.btnUp, this);//  これが通常のボタン押下と同じ呼び出し
+            this.btn.onInputOver.add(this.btnOver, this);
+            this.btn.onInputOut.add(this.btnOut, this);
+
+            this.textLabel = new Phaser.Text(game, this.btn.width / 2, this.btn.height / 2, "", {});
+            this.textLabel.anchor.set(0.5, 0.5);
+            this.add(this.textLabel);
+
+            this.resize();
+        }
+        /*  event */
+        btnDown(btn: Phaser.Button, pt: Phaser.Pointer) {
+            this.setAllFrameNo(this.downFrame);
+        }
+        btnUp(btn: Phaser.Button, pt: Phaser.Pointer) {
+            this.setAllFrameNo(this.upFrame);
+            if (this.buttonCallback != null) {
+                this.buttonCallback(btn, pt);
+            }
+        }
+        btnOver(btn: Phaser.Button, pt: Phaser.Pointer) {
+            this.setAllFrameNo(this.overFrame);
+        }
+        btnOut(btn: Phaser.Button, pt: Phaser.Pointer) {
+            this.setAllFrameNo(this.outFrame);
+        }
+
+        setAllFrameNo(no: number) {
+            for (var i = 0; i < this.splite.length; i++) {
+                this.splite[i].frame = no;
+            }
+        }
+        /* property */
+        get width() {
+            return this.btn.width;
+        }
+        set width(value) {
+            this.btn.width = value;
+            this.resize();
+        }
+        get height() {
+            return this.btn.height;
+        }
+        set height(value) {
+            this.btn.height = value;
+            this.resize();
+        }
+        get text() {
+            return this.textLabel.text;
+        }
+        set text(value) {
+            this.textLabel.text = value;
+        }
+
+        /* resize */
+        resize() {
+            //  単純化のためバウンドのサイズを計算しておく
+            var sp: Phaser.Sprite;
+            var posy = 0;
+            var posbasey = 0;
+            for (var j = 0; j < 3; j++) {
+                var posx = 0;
+                var posbasex = 0;
+                for (var i = 0; i < 3; i++) {
+                    var w: number;
+                    var wb: number;
+                    sp = this.splite[i + j * 3]; //  rectのバウンドはあとで調整する　cropで調整する場合、位置は全部同じにできるはず
+                    switch (i) {
+                        case 0: //  left
+                            w = this.baseSize.x * this.lefttopRatio.x;
+                            wb = w;
+                            break;
+                        case 2: //  right
+                            w = this.baseSize.x * this.rightbottomRatio.x;
+                            wb = w;
+                            break;
+                        default:
+                            w = this.width - this.baseSize.x * (this.lefttopRatio.x + this.rightbottomRatio.x);
+                            wb = this.baseSize.x * (1 - this.lefttopRatio.x - this.rightbottomRatio.x);
+                            break;
+                    }
+                    var h: number;
+                    var hb: number;
+                    switch (j) {
+                        case 0: //  top
+                            h = this.baseSize.y * this.lefttopRatio.y;
+                            hb = h;
+                            break;
+                        case 2: //  bottom
+                            h = this.baseSize.y * this.rightbottomRatio.y;
+                            hb = h;
+                            break;
+                        default:
+                            h = this.height - this.baseSize.y * (this.lefttopRatio.y + this.rightbottomRatio.y);
+                            hb = this.baseSize.y * (1 - this.lefttopRatio.y - this.rightbottomRatio.y);
+                            break;
+                    }
+                    sp.scale.set(w / wb, h / hb);
+                    sp.position.set(posx, posy);
+                    var r = new Phaser.Rectangle(posbasex, posbasey, wb, hb);
+                    sp.crop(r, false);
+                    posx += w;
+                    posbasex += wb;
+                    if (i === 2) {
+                        posy += h;
+                        posbasey += hb;
+
+                    }
+                    /*
+                    //  for debug
+                    for (var n = 0; n < 3; n++) {
+                        for (var m = 0; m < 3; m++) {
+                            if (m === 1 && n === 1) {
+                                this.splite[m + n * 3].alpha = 0;
+                            }
+                        }
+                    }
+*/
+                }
+            }
+
+            this.textLabel.position.set(this.btn.width / 2, this.btn.height / 2);
+        }
+    }
+
+}
---
+++ Simple9Patch/Practice1.ts
@@ -0,0 +1,77 @@
+﻿module WebSketch {
+
+    //  9patchボタン
+    export class Practice1 extends Phaser.State {
+        back: Phaser.Sprite;
+        label: Phaser.Text;
+        group:Phaser.Group;
+
+        btnOk: Button9P;
+        btnCancel: Button9P;
+
+        static loadContents(game: Phaser.Game) {
+            game.load.spritesheet("button", "assets/btn60_60_3.png", 60, 60);
+        }
+
+        create() {
+            //  背景親パネル
+            this.back = this.game.add.sprite(0, 0, "white");
+            this.back.tint = 0xffeeb9;
+
+            this.label = this.game.add.text(0, 0, "simple 9 patch", {});
+
+            this.group = this.game.add.group();
+            this.group.position.set(this.game.world.centerX, this.game.world.centerY);
+
+            //  ボタン
+
+            this.btnOk = new Button9P(this.game, -100, -20, "button", 0, () => { this.btnPush1(); }, 1, 0, 2, 0);
+            this.btnOk.text = "OK";
+            this.group.add(this.btnOk);
+            this.btnOk.width = 100;
+            this.btnOk.height = 60;
+
+            this.btnCancel = new Button9P(this.game, -10, -20, "button", 0, () => { this.btnPush2(); }, 1, 0, 2, 0);
+            this.btnCancel.text = "Cancel";
+            this.group.add(this.btnCancel);
+            this.btnCancel.width = 130;
+            this.btnCancel.height = 60;
+
+            this.resize();
+
+
+            //  5秒で遷移（これはナビゲータで管理すべきものだが）
+            //this.game.time.events.add(Phaser.Timer.SECOND * 5, this.next, this);
+        }
+
+        btnPush1() {
+            this.label.text = "OK!";
+        }
+
+        btnPush2() {
+            this.label.text = "Cancel!";
+        }
+
+        //next() {
+        //    this.game.time.events.clearPendingEvents();
+        //    this.game.state.start("Practice1", true, false);
+        //}
+
+        update() {
+        }
+
+        resize() {
+            //  背景リサイズ調整
+            this.back.width = this.game.world.width;
+            this.back.height = this.game.world.height;
+            this.group.position.set(this.game.world.centerX, this.game.world.centerY);
+        }
+
+        shutdown() {
+            //this.back.destroy();
+            //this.textblock.destroy();
+            //this.textblock2.destroy();
+        }
+    }
+
+}  
